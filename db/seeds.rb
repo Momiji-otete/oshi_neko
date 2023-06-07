a9bc54978f37b86baeb95ee46b476b532159caaa
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-07 09:09:29 +0000

    [Add]seedの記述追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  email: "admin@admin",
+  password: "111111"
+)
+
+EndUser.create!(
+  [
+    {
+      name: "猫大好き",
+      email: "neko@daisuki.com",
+      password: "123456"
+    },
+    {
+      name: "猫の奴隷",
+      email: "neko@dorei.com",
+      password: "123456"
+    }
+  ]
+)
