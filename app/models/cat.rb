cf6d9e70673a4c1a3cae518cad7b06bbb3695047
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-07 06:11:59 +0000

    [Add]アソシエーション

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,5 @@
 class Cat < ApplicationRecord
+  belongs_to :end_user
+  has_many :bookmarks, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,4 +3,11 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cats, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_cats, through: :bookmarks, source: :cat
 end
