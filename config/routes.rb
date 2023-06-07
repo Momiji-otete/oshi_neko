bf91b8d47a6752dd931ff39e727477b684069d4d
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-07 07:32:48 +0000

    [Add]ルーティングの記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,32 @@ Rails.application.routes.draw do
     sessions: "public/sessions"
   }
 
+  scope module: :pulic do
+    root :to => "homes#top"
+    get "about" => "homes#about"
+    get "end_users/withdraw_confirm" => "end_users#withdraw_confirm"
+    patch "end_users/withdraw" => "end_users#withdraw"
+    resources :end_users, only: [:show, :edit, :update]
+    resources :cats, only: [:new, :create, :show, :edit, :update] do
+      resource :bookmarks, only: [:create, :destroy]
+    end
+    resources :posts do
+      resource :likes, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+  end
+
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+   get "/" => "homes#top"
+   resources :posts, only: [:index, :show, :destroy] do
+     resources :comments, only: [:destroy]
+   end
+   resources :end_users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
