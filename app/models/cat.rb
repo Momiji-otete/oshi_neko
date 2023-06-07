0530e1a0b13f501e475f0b3ea669940f9e905063
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-07 08:02:25 +0000

    [Add]enumの定義

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,6 @@ class Cat < ApplicationRecord
   belongs_to :end_user
   has_many :bookmarks, dependent: :destroy
   has_many :posts, dependent: :destroy
+  
+  enum sex: { unanswered: 0, male: 1, female: 2 
 end
