3972b8baebe475f9041a8cc07d9d58cddec0f4db
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-08 06:52:12 +0000

    [update]show

--- app/controllers/public/cats_controller.rb
+++ app/controllers/public/cats_controller.rb
@@ -17,16 +17,21 @@ class Public::CatsController < ApplicationController
   end
 
   def edit
-
+    @cat = Cat.find(params[:id])
   end
 
   def update
-
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_path(@cat)
+    else
+      render :edit
+    end
   end
-  
-  
+
+
   private
-  
+
   def cat_params
     params.require(:cat).permit(:name, :sex, :introduction, :cat_image)
   end
