43b32674262f2668c62151aef0838584a885040d
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-08 04:47:08 +0000

    [Add]edit, update, withdraw

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -6,15 +6,30 @@ class Public::EndUsersController < ApplicationController
   end
 
   def edit
-
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to end_user_path(current_end_user)
+    else
+      render :edit
+    end
   end
 
   def withdraw_confirm
   end
 
   def withdraw
+    current_end_user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
+  private
+  
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction)
   end
 end
