e597b1b0459b9d9e6908c711e9995f1bbc4ca1e0
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-08 09:07:34 +0000

    [Add]new

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,15 @@
 class Public::PostsController < ApplicationController
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = current_end_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def index
@@ -28,5 +33,7 @@ class Public::PostsController < ApplicationController
   end
 
   private
-
+  def post_params
+    params.require(:post).permit(:title, :body, :cat_id, :image)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destory
   has_many :tags, through: :post_tags
+
+  has_one_attached :image
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
