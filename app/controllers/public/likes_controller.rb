4fb8e121e70596b61af5ed6ed3784bea76e9d71f
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-09 07:56:40 +0000

    [Update]いいねの非同期通信化、indexにいいねの追加

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,15 +1,15 @@
 class Public::LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    @like = current_end_user.likes.new(post_id: @post.id)
+    post = Post.find(params[:post_id])
+    @like = current_end_user.likes.new(post_id: post.id)
     @like.save
-    redirect_to request.referer
+    render 'replace_btn'
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @like = current_end_user.likes.find_by(post_id: @post.id)
+    post = Post.find(params[:post_id])
+    @like = current_end_user.likes.find_by(post_id: post.id)
     @like.destroy
-    redirect_to request.referer
+    render 'replace_btn'
   end
 end
