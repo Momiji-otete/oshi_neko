b0cd453fff5d00cd05307055e287b43957866c02
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-10 08:29:27 +0000

    [Add]ブックマーク機能作成

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,10 +1,14 @@
 class Public::BookmarksController < ApplicationController
   def create
-
+    cat = Cat.find(params[:cat_id])
+    current_end_user.bookmark(cat)
+    redirect_to request.referer
   end
 
   def destroy
-
+    cat = Cat.find(params[:cat_id])
+    current_end_user.drop_bookmark(cat)
+    redirect_to request.referer
   end
 
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -10,4 +10,16 @@ class EndUser < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_cats, through: :bookmarks, source: :cat
+  
+  def bookmark(cat)
+    bookmarks.create(cat_id: cat.id)
+  end
+  
+  def drop_bookmark(cat)
+    bookmarks.find_by(cat_id: cat.id).destroy
+  end
+  
+  def bookmarked?(cat)
+    bookmark_cats.include?(cat)
+  end
 end
