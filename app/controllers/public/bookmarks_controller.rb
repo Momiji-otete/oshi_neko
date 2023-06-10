95d1f6ad453553c9732dd3e1199081f662dbea27
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-10 09:26:52 +0000

    [Update]ブックマークボタンの非同期通信化

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,14 +1,16 @@
 class Public::BookmarksController < ApplicationController
   def create
     cat = Cat.find(params[:cat_id])
-    current_end_user.bookmark(cat)
-    redirect_to request.referer
+    @bookmark = current_end_user.bookmarks.new(cat_id: cat.id)
+    @bookmark.save
+    render "replace_btn"
   end
 
   def destroy
     cat = Cat.find(params[:cat_id])
-    current_end_user.drop_bookmark(cat)
-    redirect_to request.referer
+    @bookmark = current_end_user.bookmarks.find_by(cat_id: cat.id)
+    @bookmark.destroy
+    render "replace_btn"
   end
 
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,22 +3,15 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :cats, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_cats, through: :bookmarks, source: :cat
-  
-  def bookmark(cat)
-    bookmarks.create(cat_id: cat.id)
-  end
-  
-  def drop_bookmark(cat)
-    bookmarks.find_by(cat_id: cat.id).destroy
-  end
-  
+
+
   def bookmarked?(cat)
     bookmark_cats.include?(cat)
   end
