3bf11f2b4172cfee23c6049759421e063518b9d7
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-13 01:47:44 +0000

    [Add]admin/post: index,show

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,13 +1,14 @@
 class Admin::PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
-
+    Post.find(params[:id]).destroy
   end
 
 end
