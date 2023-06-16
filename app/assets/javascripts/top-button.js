3c26a25ee067fbe2a220ec0a3c28af97f6491669
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-06-16 11:29:32 +0000

    [Add]ボタンサイズ変更

--- app/assets/javascripts/top-button.js
+++ app/assets/javascripts/top-button.js
@@ -1,8 +1,8 @@
 $(function() {
   $('#back a').on('click',function(event){
     $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
+      scrollTop:0 //最上部に移動
+    }, 500);  //0.5秒間かけて
+    event.preventDefault(); //aタグの機能を無効化
   });
 });
